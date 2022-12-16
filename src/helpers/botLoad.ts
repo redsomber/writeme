25d773bda74804268fd231e9cfd054dcf9e97910
redsomber <117638415+redsomber@users.noreply.github.com>
2022-12-16 19:05:55 +0300

    Update botLoad.ts

--- src/helpers/botLoad.ts
+++ src/helpers/botLoad.ts
@@ -2,7 +2,7 @@ import { Bot, Context, SessionFlavor } from 'grammy'
 import { Conversation, ConversationFlavor } from '@grammyjs/conversations'
 import { SessionData } from '@/helpers/interfaces'
 
-const bot = new Bot<MyContext>('5859175137:AAFXwAtKjX_BsCe_JXTIKQ6y4GV2_olI72w')
+const bot = new Bot<MyContext>('env.BOT_TOKEN')
 
 export type MyContext = SessionFlavor<SessionData> &
   Context &
